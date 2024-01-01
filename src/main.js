5de7551c64ba7a22c3ac116d744ed09470ddc0d0
rauaap <c2raaa00@students.oamk.fi>
2024-01-01 08:51:34 +0200

    remove js and html files from project

--- src/main.js
+++ src/main.js
@@ -1,19 +1,64 @@
 const path = require('path');
+const fs = require('fs');
 const chokidar = require('chokidar');
 const injectScript = require('./injectScript');
 
-const watcher = chokidar.watch(path.join(__dirname, 'js'));
+const options = (() => {
+    const args = process.argv.slice(2);
+    const options = {htmlDir: null, jsDir: null}
+    let pos = 0;
 
-watcher.on('change', filePath => {
-    const fileName = path.parse(filePath).base;
-    const statusCode = injectScript(fileName);
+    for (let i = 0; i < 4; i += 2) {
+        const current = args[i];
+        const next = args[i + 1];
+
+        switch (current) {
+        case '--html':
+            options.htmlDir = next;
+            break;
+        case '--js':
+            options.jsDir = next;
+            break;
+        default:
+            return false;
+        }
+    }
+
+    return options;
+})();
+
+if (!options) {
+    console.error(
+        'Usage: node main.js --html [html file directory] --js [js file directory]'
+    );
+
+    return;
+}
+
+for (let opt of Object.values(options)) {
+    if (!fs.existsSync(opt) || !fs.statSync(opt).isDirectory()) {
+        console.error(
+            'Both --html and --js must provide existing directories.'
+        );
+
+        return;
+    }
+}
+
+const watcher = chokidar.watch(options.jsDir);
+
+watcher.on('change', jsFilePath => {
+    const fileName = path.parse(jsFilePath).name;
+    const htmlFilePath = path.join(options.htmlDir, `${fileName}.html`);
+
+    const statusCode = injectScript(jsFilePath, htmlFilePath);
 
     switch (statusCode) {
         case 0:
-            console.log(`Succesfully injected ${fileName}.`);
+            console.log(`Succesfully injected ${fileName}.js.`);
             break;
         case 1:
-            console.log(`No html file exists for ${fileName}.`);
+            console.log(`No html file exists for ${fileName}.js.`);
             break;
         default:
             break;
