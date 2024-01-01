f88ec37e0f95374895929457898c74d3d1d86736
rauaap <aapr@protonmail.com>
2024-01-01 17:36:13 +0200

    add comment stripping to make_html.py

--- tools/make_html.py
+++ tools/make_html.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, argparse
+import os, argparse, re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input-dir', default = '.', help = 'Directory containing the html files to convert into constexprs')
@@ -8,19 +8,19 @@ parser.add_argument('output_file', help = 'File to write constexprs to')
 args = parser.parse_args()
 
 html = ((h, f'{h.split(".")[0]}Html') for h in os.listdir(args.input_dir))
+comment = re.compile('<!-- .*? -->')
 
 consts = []
 for file, name in html:
     with open(os.path.join(args.input_dir, file)) as f:
         content = ''.join(
-            l.strip() for l in f.read().split('\n') if l
+            re.sub(comment, '', l.strip()) for l in f.read().split('\n') if l
         )
 
-    # Increment by one to account for the null byte
-    consts.append(f'// Length: {len(content) + 1}')
+    consts.append(f'// Length (excluding terminating null byte): {len(content)}')
     consts.append(
         f'constexpr char {name}[] = R"htmldoc({content})htmldoc";'
     )
 
 with open(args.output_file, 'w') as f:
-    f.write('\n'.join(consts))
+    f.write('\n'.join(consts) + '\n')
